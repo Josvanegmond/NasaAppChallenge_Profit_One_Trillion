f7b7df3aa45e87db23a8e51446c40af61f7f0085
Bart Enkelaar <benkelaar@gmail.com>
2014-04-13 12:41:11 +0200

    Moved profit to js folder and stuff

--- asteroids_profit_game2/js/profit.js
+++ asteroids_profit_game2/js/profit.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 	$("#create").click(function() {
-		pot.Game.getPJSObject().start();
+		var playerName = $("#name").val();
+		var playerColor = $("#colorPicker").val();
+		console.log("Clicking!");
+		if (playerName != null && playerColor != null) {
+			console.log("We're here!");
+			var pjs = pot.Game.getPJSObject();
+			pjs.start();
+//			$.ajax({
+//				url: "http://profittrillionserver.appspot.com/create",
+//				type: "POST",
+//				data: {
+//					name: playerName,
+//					color: playerColor,
+//					location: pjs.getPlayerLocation(),
+//					profit: 0}
+//				done: function(data) {
+//					pjs.setGameId(data.gameId)
+//					pollForOpponent(data.gameId);
+//				}
+//			});
+		}
 	});
 });
+
+//pollForOpponent = function(gameId) {
+//	setTimeout()
+//}
